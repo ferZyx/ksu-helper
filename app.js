f8fd2b73c6504fa92aa8618143ab70d8b374fa0f
ferZyx <sorokin.wlad@yandex.ru>
2024-01-30 15:21:50 +0600

    experiment 1

--- app.js
+++ app.js
@@ -6,40 +6,40 @@ import router from "./router.js";
 import config from "./config.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import log from "./logging/logging.js";
-import { setupLoggingPathUpdate } from "./cron/loggingPathUpdate.js";
-import { setupKsuReAuth } from "./cron/ksuReAuth.js";
+import {setupLoggingPathUpdate} from "./cron/loggingPathUpdate.js";
+import {setupKsuReAuth} from "./cron/ksuReAuth.js";
 
 const app = express();
 
+app.use(cors({
+    credentials: true,
+    origin: [
+        'https://tolyan.me',
+        'http://tolyan.me:3000',
+        'http://localhost:3000'
+    ]
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origin: [
-    'https://tolyan.me',
-    'http://tolyan.me:3000',
-    'http://localhost:3000'
-  ]
-}));
 app.use((req, res, next) => {
-  const decodedUrl = decodeURIComponent(req.url);
-  log.info(`${req.method} ${decodedUrl}`);
-  next();
+    const decodedUrl = decodeURIComponent(req.url);
+    log.info(`${req.method} ${decodedUrl}`);
+    next();
 });
-app.use('/express/api',router);
+app.use('/express/api', router);
 app.use(errorMiddleware);
 
 const appStart = async () => {
-  try {
-    await db.connect(config.DB_URI);
-    app.listen(config.PORT);
-  } catch (e) {
-    log.error("Ошибка при запуске ksu-helper" + e);
-  }
-  await setupLoggingPathUpdate();
-  await setupKsuReAuth();
+    try {
+        await db.connect(config.DB_URI);
+        app.listen(config.PORT);
+    } catch (e) {
+        log.error("Ошибка при запуске ksu-helper" + e);
+    }
+    await setupLoggingPathUpdate();
+    await setupKsuReAuth();
 };
 
 appStart().then(() =>
-  log.info(`App has been ran! http://localhost:${config.PORT}`)
+    log.info(`App has been ran! http://localhost:${config.PORT}`)
 );
