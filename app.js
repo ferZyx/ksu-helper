7e59132491c1948c01198fecdf41d25bdcd5fcd2
ferZyx <sorokin.wlad@yandex.ru>
2024-01-30 15:29:08 +0600

    experiment 4

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import {setupKsuReAuth} from "./cron/ksuReAuth.js";
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://tolyan.me");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
