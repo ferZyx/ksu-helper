e314a9b7cb5c43c923cdb4576d6b18bbf2924559
ferZyx <sorokin.wlad@yandex.ru>
2024-01-30 15:28:02 +0600

    experiment 3

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ import {setupKsuReAuth} from "./cron/ksuReAuth.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://tolyan.me');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
