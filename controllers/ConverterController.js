aa72ec4edad0c838e8ba62990e94e1e406880533
ferZyx <sorokin.wlad@yandex.ru>
2024-04-10 15:33:21 +0500

    test

--- controllers/ConverterController.js
+++ controllers/ConverterController.js
@@ -2,18 +2,14 @@ import fs from "fs";
 import multer from "multer";
 import * as uuid from "uuid";
 import queue from 'async/queue.js';
-import {wordToHtmlByLibreOffice} from "../services/ConverterService/wordToHtmlConverter.js";
+import {wordToHtmlByLibreOffice, wordToHtmlByPandoc} from "../services/ConverterService/wordToHtmlConverter.js";
 
 
 const q = queue(async (taskData) => {
     const callback = taskData.callback;
     const file = taskData.file;
     try {
-        const startTime = new Date().getTime();
-        console.log('Начало конвертации...', startTime, 'ms.');
         await wordToHtmlByLibreOffice(file.path, 'uploads/converted/');
-        const endTime = new Date().getTime();
-        console.log('Конвертация завершена.', endTime, 'ms.');
         callback(null, `Успешно сконвертирован.`);
     } catch (error) {
         callback(error, null);
@@ -46,39 +42,60 @@ export const upload = multer({storage: storage})
 
 export async function wordToHtml(req, res, next) {
     try {
-        const allowedExtensions = ['.doc', '.docx']; // Разрешенные расширения файлов
         const fileExtension = '.' + req.file.originalname.split('.').pop();
-        if (!allowedExtensions.includes(fileExtension)) {
+
+        if (fileExtension === '.doc') {
+            convertWithLibreOffice(req, res, fileExtension)
+        } else if (fileExtension === '.docx') {
+            convertWithPandoc(req, res, fileExtension).catch(e => {
+                console.log(e)
+                convertWithLibreOffice(req, res, fileExtension)
+            })
+        } else {
             return res.status(400).json({error: 'Недопустимый формат файла'});
         }
 
-        const startTime = new Date().getTime();
-        console.log('Добавление задачи в очередь...', startTime, 'ms.');
 
-        q.push({
-            file: req.file, callback: (err, result) => {
-                if (err) {
-                    console.log(err)
-                    return res.status(500).json({error: 'Ошибка конвертации файла'});
-                }
-                fs.rmSync(req.file.path); // Удаляем исходный файл
-                const htmlPath = req.file.path.replace('uploads', 'uploads/converted').replace(fileExtension, '.html');
-                const htmlAbsolutePath = `${process.cwd()}/${htmlPath}`;
-
-                const endTime = new Date().getTime();
-                console.log('Задача выполнена.', endTime, 'ms.');
-                res.sendFile(htmlAbsolutePath, (err) => {
+        function convertWithLibreOffice(req, res, fileExtension) {
+            q.push({
+                file: req.file, callback: (err, result) => {
                     if (err) {
-                        console.error('Ошибка отправки файла:', err);
-                        throw new Error('Ошибка отправки файла');
-                    } else {
-                        fs.rmSync(htmlAbsolutePath);
+                        console.log(err)
+                        return res.status(500).json({error: 'Ошибка конвертации файла'});
                     }
-                });
-            }
-        });
+                    fs.rmSync(req.file.path); // Удаляем исходный файл
+                    const htmlPath = req.file.path.replace('uploads', 'uploads/converted').replace(fileExtension, '.html');
+                    const htmlAbsolutePath = `${process.cwd()}/${htmlPath}`;
 
+                    res.sendFile(htmlAbsolutePath, (err) => {
+                        if (err) {
+                            console.error('Ошибка отправки файла:', err);
+                            throw new Error('Ошибка отправки файла');
+                        } else {
+                            fs.rmSync(htmlAbsolutePath);
+                        }
+                    });
+                }
+            })
+        }
 
+        async function convertWithPandoc(req, res, fileExtension) {
+            const htmlPath = req.file.path.replace('uploads', 'uploads/converted').replace(fileExtension, '.html');
+            const htmlAbsolutePath = `${process.cwd()}/${htmlPath}`;
+
+            await wordToHtmlByPandoc(req.file.path, htmlAbsolutePath)
+                .then(() => {
+                    fs.rmSync(req.file.path)
+                    res.sendFile(htmlAbsolutePath, (err) => {
+                        if (err) {
+                            console.error('Ошибка отправки файла:', err);
+                            throw new Error('Ошибка отправки файла');
+                        } else {
+                            fs.rmSync(htmlAbsolutePath);
+                        }
+                    })
+                })
+        }
     } catch (e) {
         next(e)
     }
